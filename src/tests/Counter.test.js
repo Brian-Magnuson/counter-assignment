b47c44db4453d53a5362ce84ec1a7121944e724d
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2024-04-06 16:05:10 -0400

    Add comment to disable eslint warning

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -6,27 +6,24 @@ import Counter from '../components/Counter';
 
 beforeEach(() => {
   // Render the Counter component here
+  // eslint-disable-next-line testing-library/no-render-in-setup
   render(<Counter />);
 })
 
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
   expect(screen.getByText('Counter')).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
   expect(screen.getByTestId('count')).toHaveTextContent('0');
 });
 
 test('clicking + increments the count', async () => {
-  // Complete the unit test below based on the objective in the line above
   userEvent.click(screen.getByText('+'));
   expect(screen.getByTestId('count')).toHaveTextContent('1');
 });
 
 test('clicking - decrements the count', async () => {
-  // Complete the unit test below based on the objective in the line above
   userEvent.click(screen.getByText('-'));
   expect(screen.getByTestId('count')).toHaveTextContent('-1');
 });
